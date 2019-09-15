a51821ea5e2444b0d11b6ce6ae479f1e3e82b3dd
qiang <jhq0113@163.com>
2019-09-15 20:47:56 +0800

    调试协议配置

--- rest/application/modules/V1/controllers/Index.php
+++ rest/application/modules/V1/controllers/Index.php
@@ -32,7 +32,24 @@ class IndexController extends \Yaf\Controller_Abstract
          */
         $boot = Container::get('boot');
         $result['getList'] = $boot->call('getList',[
-            'name' => 'getList'
+            'name' => 'getList',
+            'data' => '粘包测试adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data0' => 'adfasdfasfdasfasdfasfdasdfa啥都发发阿斯顿sdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data1' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdf撒旦法阿萨德萨芬安抚按时asfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data2' => 'adfasdfasfdasfasdfasfdasdfasdf挺好的asdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data3' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data4' => 'adfasdfasfdasfasdfasfdasdfasdfas我也是dfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data5' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfd撒旦法撒旦法是发asdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data6' => 'adfasdfasfdasfasdfasfda所发生的啊sdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data7' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data8' => 'adfasdfasfdasfasdfasfd是的发生按时asdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data9' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data10' => 'adfasdfasfdasfasdfasfd撒旦法asdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data11' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data12' => 'adfasdfasfdasfasdfas是啊啊 ad方法fdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data13' => 'adfasdfasfdasfasdfasfdasdfasdfasdfas士大夫撒旦法dfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data14' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data15' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfa大撒旦法收到sdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
         ]);
 
         $result['func'] = $boot->call('func',[
