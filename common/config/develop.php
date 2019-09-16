45a3e9a671f5870725ac5629a228af5a5b51c41b
Jiang Haiqiang <jhq0113@163.com>
2019-09-16 19:26:14 +0800

    add redis cache

--- common/config/develop.php
+++ common/config/develop.php
@@ -20,6 +20,26 @@ $develop = [
         ],
         'yac' => [
             'class' => 'cockroach\cache\Yac'
+        ],
+        'redis' => [
+            'class'   => 'cockroach\cache\Redis',
+            //负载算法为根据ip做一致性hash
+            'servers' => [
+                [
+                    "host"    => "10.16.49.100",
+                    "port"    => 13006,
+                    "auth"    => "browser@360",
+                    'db'      => 1,
+                    'timeout' => 3,
+                ],
+                [
+                    "host"    => "10.203.151.157",
+                    "port"    => 1222,
+                    "auth"    => "71ad83e43c8252af",
+                    'db'      => 1,
+                    'timeout' => 3,
+                ],
+            ]
         ]
     ]
 ];
---
+++ rest/application/modules/V1/controllers/Boot.php
@@ -0,0 +1,51 @@
+<?php
+
+use cockroach\base\Container;
+
+/**
+ * Class BootController
+ * @datetime 2019/9/16 18:31
+ * @author roach
+ * @email jhq0113@163.com
+ */
+class BootController extends \Yaf\Controller_Abstract
+{
+    /**
+     * @datetime 2019/9/13 17:28
+     * @author roach
+     * @email jhq0113@163.com
+     */
+    public function bootAction()
+    {
+        /**
+         * @var \cockroach\client\SwooleBoot $boot
+         */
+        $boot = Container::get('boot');
+        $result['getList'] = $boot->call('getList',[
+            'name' => 'getList',
+            'data' => '粘包测试adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data0' => 'adfasdfasfdasfasdfasfdasdfa啥都发发阿斯顿sdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data1' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdf撒旦法阿萨德萨芬安抚按时asfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data2' => 'adfasdfasfdasfasdfasfdasdfasdf挺好的asdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data3' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data4' => 'adfasdfasfdasfasdfasfdasdfasdfas我也是dfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data5' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfd撒旦法撒旦法是发asdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data6' => 'adfasdfasfdasfasdfasfda所发生的啊sdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data7' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data8' => 'adfasdfasfdasfasdfasfd是的发生按时asdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data9' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data10' => 'adfasdfasfdasfasdfasfd撒旦法asdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data11' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data12' => 'adfasdfasfdasfasdfas是啊啊 ad方法fdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data13' => 'adfasdfasfdasfasdfasfdasdfasdfasdfas士大夫撒旦法dfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data14' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfasdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+            'data15' => 'adfasdfasfdasfasdfasfdasdfasdfasdfasdfa大撒旦法收到sdfasfdasdfasfdasdfasdfasdfsdfasdfasfdasdfasfdasdfasdfasdfasfdasdfasdfasdfasfdasf',
+        ]);
+
+        $result['func'] = $boot->call('func',[
+            'name' => 'func'
+        ]);
+
+        exit(json_encode($result,JSON_UNESCAPED_UNICODE));
+    }
+}
